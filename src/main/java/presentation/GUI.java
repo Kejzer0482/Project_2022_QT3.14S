2d581ab80102d4ee317ca5d62d07efdb056a78ce
Kejzer <118442013+Kejzer0482@users.noreply.github.com>
2022-11-24 14:00:11 +0100

    Update GUI.java

--- src/main/java/presentation/GUI.java
+++ src/main/java/presentation/GUI.java
@@ -1,5 +1,16 @@
 package presentation;
 
-public class GUI {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class GUI extends Application {
+    public static void main(String[] args) {
+        //Launch(args);
+    }
+
+    //@Override
+    public void start(Stage stage) /*throws Exception*/ {
+
+        stage.show();
+    }
 }
