24b29021d69103d909e56a8ccbc0f0002483e580
Kejzer <Kaare.Jensen@live.dk>
2022-11-29 13:39:50 +0100

    GUI Start

---
+++ src/main/java/presentation/Controller.java
@@ -0,0 +1,22 @@
+package presentation;
+
+import java.awt.event.ActionEvent;
+
+public class Controller {
+
+    public void up(ActionEvent e) {
+        System.out.println("UP");
+    }
+    public void down(ActionEvent e) {
+        System.out.println("DOWN");
+    }
+    public void right(ActionEvent e) {
+        System.out.println("RIGHT");
+    }
+    public void left(ActionEvent e) {
+        System.out.println("LEFT");
+    }
+    public void close(ActionEvent e) {
+        System.out.println("LEFT");
+    }
+}
--- src/main/java/presentation/Main.java
+++ src/main/java/presentation/Main.java
@@ -1,11 +1,41 @@
 package presentation;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, 600, 600, Color.LAVENDER);
+        Stage stage = new Stage();
+
+        Text text = new Text();
+        text.setText("Whoa");
+        text.setX(50);
+        text.setY(50);
+        text.setFont(Font.font("Calibri", 50));
+        text.setFill(Color.BLACK);
+
+        Line line = new Line();
+        line.setStartX(200);
+        line.setStartY(200);
+        line.setEndX(500);
+        line.setEndY(200);
+        line.setStrokeWidth(5);
+        line.setStroke(Color.RED);
+        line.setOpacity(0.5);
+        line.setRotate(45);
+
+        root.getChildren().add(text);
+        root.getChildren().add(line);
+        stage.setScene(scene);
         stage.show();
     }
 
